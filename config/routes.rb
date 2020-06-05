2ec0b84af93c6cbac681ee36821680c2e1a25ee7
lmaser <luismartinserradilla@gmail.com>
2020-06-05 03:45:32 -0700

    Page layout added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+  # we define a root path with store_index-> tells Rails to create store_index_path and store_index_url accessor methods
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
