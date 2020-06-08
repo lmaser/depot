6aefb0d4c9f16ea841b7af1acf391c8867927d80
lmaser <luismartinserradilla@gmail.com>
2020-06-08 03:43:22 -0700

    Caching of partial results in dev environment and modle title uniqueneess fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,11 @@
 class Product < ApplicationRecord
-    validates :title ,length: { maximum: 50 }, presence: true
-    validates :description, :image_url, presence: true
-    validates :price, numericality: { greater_than_or_equal_to: 0.01}
+    validates :title, :description, :image_url, presence: true
     validates :title, uniqueness: true
-
     validates :image_url, allow_blank: true, format: {
-        with: %r{\.(gif|jpg|png)\z}i,
-        message: 'must be a URL for GIF,JPG or PNG image.'
+      with:    %r{\.(gif|jpg|png)\z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'
     }
-
-
-end
+    validates :title, length: {minimum: 10}
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  
+  end
