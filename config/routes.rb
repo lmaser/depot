6f96eb55adf62c64162eb8009db3a03b88877616
lmaser <luismartinserradilla@gmail.com>
2020-06-09 06:34:31 -0700

    Cart scaffold added, relationships between cart, list_item and products created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   root 'store#index', as: 'store_index'
   # we define a root path with store_index-> tells Rails to create store_index_path and store_index_url accessor methods
   resources :products
